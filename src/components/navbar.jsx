cfe9e05250aad412c33295a1b5610886f16998c9
KUNAL CHOURE <chourekunal4@gmail.com>
2024-08-18 13:14:53 +0530

    added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+
+export default function navbar(){
+
+     return (
+        <>
+          <div className="nav">
+            <div className="heading">
+                <h1>BookAFeast</h1>
+            </div>
+            <div className="serach">
+
+            </div>
+            <div className="user">
+                <button>Sign in</button>
+                <button>Log in</button>
+            </div>
+          </div>
+        </>
+     )
+}
