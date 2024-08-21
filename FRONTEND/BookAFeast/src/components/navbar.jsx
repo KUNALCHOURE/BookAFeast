093cf24c796deb4d41d74d54bb419bd7c3a75e51
KUNAL CHOURE <chourekunal4@gmail.com>
2024-08-21 21:37:17 +0530

    added

--- FRONTEND/BookAFeast/src/components/navbar.jsx
+++ FRONTEND/BookAFeast/src/components/navbar.jsx
@@ -13,7 +13,12 @@ export default function Navbar(){
                 <input type="text"  placeholder='Search here' />
                 </div>
                 <div className='button rounded-3xl p-0.5'>
+                  <span className='mr-2'>
                 <button>Search</button>
+                </span>
+                <span>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                </span>
                </div>
 
             </div>
